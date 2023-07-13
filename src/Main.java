480867ae8a2adc78e483a35c82dbe9d34803c1e6
iTeRRoR404 <sbuzin@bk.ru>
2023-07-13 20:26:22 +0300

    MiFirstCommit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 3, 4, 5, 6, 7, 8, 9, 10};
+        int max = 0;
+        int min = 1;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > max){
+                max = array[i];
+            }
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+            System.out.println(min);
+            System.out.println(max);
+
+
+    }
+}
